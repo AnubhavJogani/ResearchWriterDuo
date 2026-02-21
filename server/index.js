4e0c37937a5ed92a471464a8cb5570517072f98e
Anubhav <anubhavjogani@gmail.com>
2026-02-21 22:42:15 +0530

    fix issue

--- server/index.js
+++ server/index.js
@@ -72,5 +72,8 @@ app.post('/api/createPost', async (req, res) => {
 
 
 
-app.listen(process.env.PORT || 3001, '0.0.0.0', () => {  console.log(`Server is running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
